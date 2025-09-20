d97d7e339de0d3e72270282f74af483eaff24476
mennahelsheikh <mennahelsheikh@gmail.com>
2025-09-20 03:14:57 +0300

    leetcode sql 50

---
+++ Leetcode/1633. Percentage of Users Attended a Contest.sql
@@ -0,0 +1,6 @@
+/* Write your T-SQL query statement below */
+select distinct contest_id , round(count(*) * 100.00/ (select count( distinct user_id) from Users ), 2 )  as percentage 
+from Register 
+group by contest_id 
+order by percentage desc, contest_id asc ;  
+-- 1633. Percentage of Users Attended a Contest
