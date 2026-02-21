c0fa2b1bc7f85c20b47aa79966e11e6e90c8bfd2
mennahelsheikh <mennahelsheikh@gmail.com>
2026-02-21 15:04:31 +0200

    solved

---
+++ Leetcode/619. Biggest Single Number.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+select max(num) as num
+from(
+    select num
+from MyNumbers 
+group by num 
+having count(num) <= 1 
+)t 
+
+-- 619. Biggest Single Number
